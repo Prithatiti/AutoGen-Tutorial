05dc8876e445d6d39f20ea6f8608daf4d791902b
Pritha Majumder <prithatiti@gmail.com>
2025-05-02 08:51:59 +0530

    update

---
+++ 1. Introduction/assis_userprox_agent.py
@@ -0,0 +1,33 @@
+import os
+from autogen import AssistantAgent, UserProxyAgent
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = "gpt-3.5-turbo"
+
+llm_config = {
+    "model": model,
+    "api_key": os.environ.get("OPENAI_API_KEY")
+}
+
+assitant = AssistantAgent(
+    name = "assistant_agent"
+    llm_config = llm_config,
+)
+
+user_proxy = UserProxyAgent(
+    name = "user_proxy_agent",
+    llm_config = llm_config,
+    code_execution_config = {
+        "workd_dir": "Code Execution",
+        "use_docker": False
+    },
+    human_input_mode = "NEVER"
+)
+
+# Start the agent
+user_proxy.initiate_chat(
+    assitant,
+    message = "What is the capital of the France?"
+)
